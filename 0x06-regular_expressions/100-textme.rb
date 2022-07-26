896f025379381944d50f0a102f01063eef0a38ec
chinomso okoye <chinomsokoye@yahoo.com>
2022-07-26 06:01:47 -0700

    Match sender, receiever, flags in textme

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,4 +1,4 @@
 #!/usr/bin/env ruby
 # Matches sender, receiver, flags in textme
 
-puts ARGV[0].scan(/\[from:(.*?)\]\s\[to:(.*?)\]\s\[flags:(.*?)\]\).join(',')
+puts ARGV[0].scan(/(?<=from:)\S*(?<!\])|(?<=to:).?\d{10,11}|(?<=flags:)\S*(?<!\])/).join(',')
