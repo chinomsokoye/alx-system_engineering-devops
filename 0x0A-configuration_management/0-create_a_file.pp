edf52a94c80ac91ac50ff7808ca5e0dfa4152a28
chinomso okoye <chinomsokoye@yahoo.com>
2022-09-16 06:22:36 -0700

    Create a file in /tmp

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,10 @@
 # Create a file in /tmp with specifications below
 
 file { '/tmp/school':
-ensure => 'file',
-mode => '0744,
-owner => 'www-data',
-group => 'www.data',
-content => 'I love Puppet',
+  ensure  =>  file,
+  path    =>  '/tmp/school',
+  mode    =>  '0744',
+  owner   =>  'www-data',
+  group   =>  'www-data',
+  content =>  'I love Puppet'
 }
