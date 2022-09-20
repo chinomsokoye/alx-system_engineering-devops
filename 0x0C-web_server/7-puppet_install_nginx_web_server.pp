3288c8bc8857364a6020673dad2ccc93bb0521b2
chinomso okoye <chinomsokoye@yahoo.com>
2022-09-20 12:04:38 -0700

    Install Nginx web server using Puppet

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,20 +1,6 @@
-# Puppet manifest to install nginx
-package { 'nginx':
-  ensure  =>  installed,
-}
+# Puppet manifest to install nginx web server
 
-file_line { 'aaaa':
-  ensure =>  'present',
-  path   =>  '/etc/nginx/sites-available/default',
-  after  =>  '/listen 80 default_server;',
-  line   =>  'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
-}
-
-file { '/var/www/html/index.html':
-  content  =>  'Hello World!',
-}
-
-service { 'nginx':
-  ensure  =>  running,
-  require =>  Package['nginx'],
+exec { 'server configuration':
+  provider =>  shell,
+  command  =>  'sudo apt-get -y update; sudo apt-get -y install nginx; echo "Hello World!" >  /var/www/html/index.html; sudo sed -i "server_name _;/a location /redirect_me {\\n\\treturn 301 https://google.com;\\n\\t}\\n" /etc/nginx/sites-available/default; sudo service nginx restart',
 }
