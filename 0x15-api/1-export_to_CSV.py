734dafc52964d1844376f21071ba9a34e2183db3
chinomso okoye <chinomsokoye@yahoo.com>
2022-10-17 14:16:39 -0700

    Export data in the CSV format

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -6,30 +6,16 @@ import requests
 from sys import argv
 
 
-def export_csv():
-    """ Using REST API, returns TODO list progress """
-    employee = requests.get("https://jsonplaceholder.typicode.com/users")
-    for e in employee.json():
-        if e.get('id') == int(argv[1]):
-            USERNAME = (e.get('username'))
-            break
-    TASK_STATUS_TITLE = []
-    todo_list = requests.get("https://jsonplaceholder.typicode.com/todos")
-    for tl in todo_list.json():
-        if tl.get('userId') == int(argv[1]):
-            TASK_STATUS_TITLE.append((tl.get('completed'), tl.get('title')))
-
-    filename = "{}.csv".format(argv[1])
-    with open(filename, "w") as csvfile:
-        fieldnames = ["USER_ID", "USERNAME",
-                      "TASK_COMPLETED_STATUS", "TASK_FILE"]
-        writer = csv.DictWriter(csvfile, fieldnames=fieldnames,
-                                quoting=csv.QUOTE_ALL)
-        for task in TASK_STATUS_TITLE:
-            writer.writerow({"USER_ID": argv[1], "USERNAME": USERNAME,
-                             "TASK_COMPLETED_STATUS": task[0],
-                             "TASK_TITLE": task[1]})
-
-
 if __name__ == "__main__":
-    export_csv()
+    user_id = argv[1]
+    url = "https://jsonplaceholder.typicode.com/users/{}".format(
+        user_id)
+    user = requests.get(url, verify=False).json()
+    url = "https://jsonplaceholder.typicode.com/todos?userId={}".format(
+        user_id)
+    todo = requests.get(url, verify=False).json()
+    with open("{}.csv".format(user_id), "w", newline="") as csvfile:
+        taskwriter = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
+        for t in todo:
+            taskwriter.writerow([int(user_id), user.get('username'),
+                                 t.get('completed'), t.get('title')])
