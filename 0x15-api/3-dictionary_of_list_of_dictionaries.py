1eae96b797baeb58d25b61ac76574e656c70988e
chinomso okoye <chinomsokoye@yahoo.com>
2022-10-17 15:07:07 -0700

    Export data in the JSON (dict) format

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -8,18 +8,18 @@ import requests
 if __name__ == "__main__":
     url = "https://jsonplaceholder.typicode.com/users"
     users = requests.get(url, verify=False).json()
-    userdoc = {}
-    urldoc = {}
+    usdoc = {}
+    udoc = {}
     for user in users:
         user_id = user.get("id")
-        urldoc[user_id] = []
-        userdoc[user_id] = user.get("username")
-        url = "https://jsonplaceholder.typicode.com/todos"
-        todo = requests.get(url, verify=False).json()
-        [urldoc.get(tl.get("userId")).append({"task": tl.get("title"),
-                                              "completed": tl.get("completed"),
-                                              "username": userdoc.get(
-                                                  tl.get("userId"))})
-         for tl in todo]
-        with open("todo_all_employees.json", "w") as jsonfile:
-            json.dump(urldoc, jsonfile)
+        udoc[user_id] = []
+        usdoc[user_id] = user.get("username")
+    url = "https://jsonplaceholder.typicode.com/todos"
+    todo = requests.get(url, verify=False).json()
+    [udoc.get(tl.get("userId")).append({"task": tl.get("title"),
+                                        "completed": tl.get("completed"),
+                                        "username": usdoc.get(
+                                            tl.get("userId"))})
+     for tl in todo]
+    with open("todo_all_employees.json", "w") as jsonfile:
+        json.dump(udoc, jsonfile)
