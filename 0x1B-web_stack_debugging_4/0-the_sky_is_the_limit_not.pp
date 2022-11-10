8032ad5071459c99ae9e627b73e7bc50c67071a2
chinomso okoye <chinomsokoye@yahoo.com>
2022-11-10 04:58:32 -0800

    Fix a file

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,6 +1,5 @@
 # Fixes a file
 exec {'sets file limit for nginx':
-  command =>  'sed -i "s/15/2000/g" /etc/default/nginx',
-  path    =>  '/bin/:/sbin/:/usr/bin/:/usr/sbin/',
-  onlyif  =>  'test -f /etc/default/nginx'
+  command =>  'sed -i "s/15/10000/" /etc/default/nginx && sudo service nginx restart',
+  path    =>  '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games',
 }
